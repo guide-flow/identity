bbac98b4da84ef7f54f3bac61e967ca427b26c3c
Vasilije Kiso <kisovasilije46@gmail.com>
2025-07-21 23:15:00 +0200

    registration - main web api name change

---
+++ Identity/Identity.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+
+}
